2b0ce80a1a7dc9e2af13318aa154b2e01a529665
Alex <alexfilsecker@gmail.com>
2023-12-10 01:41:53 -0300

    first problem

---
+++ 1_Arrays&Hashing/217_contains_duplicate.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution(object):
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hash = {}
+        for num in nums:
+            if num in hash:
+                return True
+            else:
+                hash[num] = True
+        
+        return False
+        
+
+if __name__ == "__main__":
+    nums = [1,2,3,1]
+    print(Solution().containsDuplicate(nums))
