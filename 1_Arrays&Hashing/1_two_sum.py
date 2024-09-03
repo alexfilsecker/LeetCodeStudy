d7501dd0e66073d769d3a8236c69285e6619a46d
Alex <alexfilsecker@gmail.com>
2024-09-03 15:50:51 +1000

    optimizied two sum

--- 1_Arrays&Hashing/1_two_sum.py
+++ 1_Arrays&Hashing/1_two_sum.py
@@ -1,19 +1,18 @@
 """easy"""
 
-
 from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, num_left in enumerate(nums):
-            sub_nums = nums[i + 1 :]
-            for j, num_right in enumerate(sub_nums):
-                if num_left + num_right == target:
-                    return [i, j + i + 1]
+        hash = {nums[i]: i for i in range(len(nums))}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hash and hash[complement] != i:
+                return [i, hash[target - num]]
 
 
 if __name__ == "__main__":
-    nums = [2, 7, 11, 15]
-    target = 9
+    nums = [3, 2, 4]
+    target = 6
     print(Solution().twoSum(nums, target))
