9645d89b6d65048dc9462003288060493c903dc2
Alex <alexfilsecker@gmail.com>
2024-09-03 16:31:38 +1000

    second try valid anagram

--- 1_Arrays&Hashing/242_valid_anagram.py
+++ 1_Arrays&Hashing/242_valid_anagram.py
@@ -1,4 +1,4 @@
-"""easy"""
+"""easy 2"""
 
 from typing import Dict
 
@@ -7,33 +7,29 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
+        # hash s
+        hash: Dict[str, int] = {}
+        for char in s:
+            if char in hash:
+                hash[char] += 1
+            else:
+                hash[char] = 1
 
-        def hash_str(string: str) -> Dict[str, int]:
-            char_hash: Dict[str, int] = {}
-            for char in string:
-                if char in char_hash:
-                    char_hash[char] += 1
-                else:
-                    char_hash[char] = 1
-
-            return char_hash
-
-        s_char_hash = hash_str(s)
-        t_char_hash = hash_str(t)
-
-        letters = set(s_char_hash.keys()).union(set(t_char_hash.keys()))
-
-        for letter in letters:
-            try:
-                if s_char_hash[letter] != t_char_hash[letter]:
-                    return False
-            except KeyError:
+        # Iterate over t
+        print(hash)
+        for char in t:
+            if char not in hash:
+                print("char", char, "not in hash")
                 return False
+            if hash[char] == 0:
+                return False
+
+            hash[char] -= 1
 
         return True
 
 
 if __name__ == "__main__":
     s = "anagran"
-    t = "nagaram"
+    t = "nagaran"
     print(Solution().isAnagram(s, t))
