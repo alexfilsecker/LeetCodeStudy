d2dee6d1576f46062f4ea2075788be83d26f9163
Alex <alexfilsecker@gmail.com>
2024-09-07 14:51:53 +1000

    2 binary search

--- 4_BinarySearch/704_binary_search.py
+++ 4_BinarySearch/704_binary_search.py
@@ -1,27 +1,25 @@
-"""easy"""
+"""easy 2"""
 
 from typing import List
 
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        low = 0
-        high = len(nums) - 1
-        while low <= high:
-            mid = low + (high - low) // 2
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            print(*(nums[i] for i in [left, mid, right]))
             if nums[mid] == target:
                 return mid
-
-            if nums[mid] < target:
-                low = mid + 1
-
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                high = mid - 1
+                right = mid - 1
 
         return -1
 
 
 if __name__ == "__main__":
-    nums = [-1, 0, 3, 5, 9, 12]
+    nums = [5]
     target = 2
     print(Solution().search(nums, target))
