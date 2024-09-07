ef586390fbf3a14c3d86429f8a32f70f56ccf0b6
Alex <alexfilsecker@gmail.com>
2024-09-07 15:31:49 +1000

    list utils module

--- 5_LinkedList/206_reverse_linked_list.py
+++ 5_LinkedList/206_reverse_linked_list.py
@@ -1,39 +1,7 @@
 """easy"""
 
 from typing import List, Optional
-
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-    def __repr__(self):
-        return f"Node = {self.val}"
-
-
-def recursive_create(values: List[int], index: int) -> ListNode:
-    node = ListNode()
-    node.val = values[index]
-    if index < len(values) - 1:
-        node.next = recursive_create(values, index + 1)
-
-    return node
-
-
-def view_list(head: ListNode):
-    node = head
-    node_list = []
-    while True:
-        if node is None:
-            break
-        node_list.append(node.val)
-        if node.next is None:
-            break
-        node = node.next
-
-    print(node_list)
+from list_utils import ListNode, recursive_create, view_list
 
 
 class Solution:
---
+++ 5_LinkedList/list_utils.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return f"Node = {self.val}"
+
+
+def recursive_create(values: List[int], index: int) -> ListNode:
+    node = ListNode()
+    node.val = values[index]
+    if index < len(values) - 1:
+        node.next = recursive_create(values, index + 1)
+
+    return node
+
+
+def view_list(head: ListNode):
+    node = head
+    node_list = []
+    while True:
+        if node is None:
+            break
+        node_list.append(node.val)
+        if node.next is None:
+            break
+        node = node.next
+
+    print(node_list)
