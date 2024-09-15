dbb64e0e4b5235cbf5359b187b44838c785838da
Alex <alexfilsecker@gmail.com>
2024-09-15 14:32:16 +1000

    contains duplicate better comments

--- 1_Arrays&Hashing/217_contains_duplicate.py
+++ 1_Arrays&Hashing/217_contains_duplicate.py
@@ -1,11 +1,23 @@
-"""easy 2"""
+"""
+Dificulty: Easy 
+Times Completed: 2
+Link: https://leetcode.com/problems/contains-duplicate/description/
+Basic Description: Check if an array of nums has a duplicated value
+"""
 
 from typing import List
 
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        """
+        Create a hash of nums to be able to check in O(1) if we have already
+        seen that number. Then traverse all the list updating the hash
+        """
+
+        # A set is the same as a hash of { key: True }
         hash = set()
+
         for num in nums:
             if num in hash:
                 return True
