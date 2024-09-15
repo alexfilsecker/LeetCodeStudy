ef033dc66d55e392ea4415a99b130a992bd198d0
Alex <alexfilsecker@gmail.com>
2024-09-15 21:43:22 +1000

    product of array except self with better coments

--- 1_Arrays&Hashing/238_product_of_array_except_self.py
+++ 1_Arrays&Hashing/238_product_of_array_except_self.py
@@ -1,22 +1,33 @@
-"""medium"""
+"""
+Dificulty: Medium
+Times Completed: 2
+Link: https://leetcode.com/problems/product-of-array-except-self/description/
+Basic Description:
+    Given nums, return an array in wich all every element is equal to the
+    product of all other numbers except self. You must do it without division
+"""
 
 from typing import List
 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = 1
-        postfix = 1
+        """
+        We calculate a prefix list and a sufix list.
+        prefix[i] is the product of all numbers before i
+        and sufix[i] is the product of all numbers after i
+        """
         n = len(nums)
-        result = [1] * n
-        for i in range(n):
-            result[i] = prefix
-            prefix *= nums[i]
-        for i in range(n - 1, -1, -1):
-            result[i] *= postfix
-            postfix *= nums[i]
+        prefix = [1] * n
+        sufix = [1] * n
 
-        return result
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            sufix[i] = sufix[i + 1] * nums[i + 1]
+
+        return [prefix[i] * sufix[i] for i in range(n)]
 
 
 if __name__ == "__main__":
