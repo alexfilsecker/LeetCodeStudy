db80fd1131f5f777a417eb114854ea93b02afd12
Alex <alexfilsecker@gmail.com>
2024-09-15 14:28:28 +1000

    better comments for two sum

--- 1_Arrays&Hashing/1_two_sum.py
+++ 1_Arrays&Hashing/1_two_sum.py
@@ -1,7 +1,8 @@
 """
-difficulty: easy
-completed times: 2
-link: https://leetcode.com/problems/two-sum/description/
+Difficulty: Easy
+Times Completed: 2
+Link: https://leetcode.com/problems/two-sum/description/
+Basic Description: Return the two numbers in an array that sum a target number
 """
 
 from typing import List
@@ -9,9 +10,24 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        The strategy is to hash the array in the form of "value: index". Then
+        we traverse the array, calculating what is the complement needed for that
+        number to be part of the result and we check if that complement is in
+        our hash.
+        """
+
+        # Create the hash "value: index" for the array of nums
         hash = {nums[i]: i for i in range(len(nums))}
+
+        # Traverse the array
         for i, num in enumerate(nums):
+
+            # Calculate the complement
             complement = target - num
+
+            # Check if that complement exists and it is not the same
+            # number we are currently seing
             if complement in hash and hash[complement] != i:
                 return [i, hash[target - num]]
 
